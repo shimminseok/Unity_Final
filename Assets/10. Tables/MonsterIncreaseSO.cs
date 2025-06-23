1d837bb9b4904d4d32afc644e9749b14a950d039
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-06-23 21:07:00 +0900

    feat : StageTable 및 몬스터 테이블, MonsterIncrease데이터 추가
    - StageTable 및 몬스터 테이블, MonsterIncrease데이터 추가

---
+++ Assets/10. Tables/MonsterIncreaseSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewMonsterIncreaseSO", menuName = "ScriptableObject/MonsterIncrease/MonsterIncrease", order = 0)]
+public class MonsterIncreaseSO : ScriptableObject
+{
+    public List<StatData> IncreaseStats;
+}
