849785d8725ca74209d7547bb14a8d130ff69816
yoonjin1127 <129118497+yoonjin1127@users.noreply.github.com>
2025-06-25 00:38:24 +0900

    feat: 덱빌딩 UI 기초작업
    덱빌딩 UI 기초작업

---
+++ Assets/2. Scripts/Yoojin/Deck/PlayerDeck.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerDeck
+{
+    // 선택한 카드덱
+    [SerializeField]
+    public List<EntryDeckData> deckDatas = new List<EntryDeckData>();
+}
