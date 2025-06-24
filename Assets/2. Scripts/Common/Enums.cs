8cb4567ec7a378256e04faa38b4f16f4cc115a46
OHSUHO <suho.oh1213@gmail.com>
2025-06-24 10:59:40 +0900

    Suho/feat/jobType추가, 스킬SO수정
    jobType추가, 스킬SO수정

--- Assets/2. Scripts/Common/Enums.cs
+++ Assets/2. Scripts/Common/Enums.cs
@@ -68,3 +68,47 @@ public enum Emotion
     Depression, //우울
     Joy         // 기쁨
 }
+
+
+
+/*SelectTargetType : 스킬을 사용할 때 적을 선택하는 로직 타입
+ * EnemySingle : 적 한명
+ * EnemyTwoRandom : 적 한 명 선택, 한 명 랜덤
+ * EnemyAll : 적 모두
+ * PlayerSingle : 플레이어 한 명
+ * PlayerTwoRandom : 플레이어 한 명 선택, 한 명 랜덤
+ * PlayerAll : 플레이어 전체
+ */
+public enum SelectTargetType
+{
+    EnemySingle,
+    //EnemyConShaped,
+    EnemyTwoRandom,
+    EnemyAll,
+    PlayerSingle,
+    PlayerTwoRandom,
+    //PlayerConShaped,
+    PlayerAll,
+}
+
+/*
+ *  Male_Warrior : 남자기사
+    FeMale_Warrior : 여자기사
+    SpearMan : 창술사
+    DragonKnight : 용기사
+    Archer : 궁수
+    Priest : 성직자
+    Mage : 마법사
+ *
+ */
+
+public enum JobType
+{
+    Male_Warrior,
+    FeMale_Warrior,
+    SpearMan,
+    DragonKnight,
+    Archer,
+    Priest,
+    Mage
+}
