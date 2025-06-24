dac44ec9dee1e6252470bbe462235ab78734c92a
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-06-24 10:04:28 +0900

    feat : Inventory 추가, ItemSO 추가
    - Inventory 추가
    - ItemSO 추가

---
+++ Assets/10. Tables/ItemSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class ItemSO : ScriptableObject
+{
+    public int ID;
+    public string ItemName;
+    public string ItemDescription;
+    public ItemType ItemType;
+    public Sprite ItemSprite;
+    public int ItemGrade;
+}
