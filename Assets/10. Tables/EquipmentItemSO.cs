aaafc4d13f638f5920d91978337196804865a240
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-06-24 11:09:15 +0900

    feat : Item별 전용 Class 타입 추가
    - Item별 전용 Class 타입 추가

--- Assets/10. Tables/EquipmentItemSO.cs
+++ Assets/10. Tables/EquipmentItemSO.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewEquipmentData", menuName = "ScriptableObject/Item/Equipment", order = 0)]
 public class EquipmentItemSO : ItemSO
 {
+    public JobType JobType;
     public EquipmentType EquipmentType;
     public List<StatData> Stats;
 }
