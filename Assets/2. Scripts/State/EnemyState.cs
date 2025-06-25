e3c274cc1ea0bd1d72a53617e680dd5192ae38e7
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-06-25 11:07:14 +0900

    feat : EnemyState추가

---
+++ Assets/2. Scripts/State/EnemyState.cs
@@ -0,0 +1,122 @@
+﻿namespace EnemyState
+{
+    public class IdleState : IState<EnemyUnitController, EnemyUnitState>
+    {
+        public void OnEnter(EnemyUnitController owner)
+        {
+        }
+
+        public void OnUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnFixedUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnExit(EnemyUnitController entity)
+        {
+        }
+
+        public EnemyUnitState CheckTransition(EnemyUnitController owner)
+        {
+            return EnemyUnitState.Idle;
+        }
+    }
+
+    public class MoveState : IState<EnemyUnitController, EnemyUnitState>
+    {
+        public void OnEnter(EnemyUnitController owner)
+        {
+        }
+
+        public void OnUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnFixedUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnExit(EnemyUnitController entity)
+        {
+        }
+
+        public EnemyUnitState CheckTransition(EnemyUnitController owner)
+        {
+            return EnemyUnitState.Idle;
+        }
+    }
+
+    public class AttackState : IState<EnemyUnitController, EnemyUnitState>
+    {
+        public void OnEnter(EnemyUnitController owner)
+        {
+        }
+
+        public void OnUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnFixedUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnExit(EnemyUnitController entity)
+        {
+        }
+
+        public EnemyUnitState CheckTransition(EnemyUnitController owner)
+        {
+            return EnemyUnitState.Idle;
+        }
+    }
+
+    public class DeadState : IState<EnemyUnitController, EnemyUnitState>
+    {
+        public void OnEnter(EnemyUnitController owner)
+        {
+        }
+
+        public void OnUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnFixedUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnExit(EnemyUnitController entity)
+        {
+        }
+
+        public EnemyUnitState CheckTransition(EnemyUnitController owner)
+        {
+            return EnemyUnitState.Idle;
+        }
+    }
+
+    public class StunState : IState<EnemyUnitController, EnemyUnitState>
+    {
+        public void OnEnter(EnemyUnitController owner)
+        {
+        }
+
+        public void OnUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnFixedUpdate(EnemyUnitController owner)
+        {
+        }
+
+        public void OnExit(EnemyUnitController entity)
+        {
+        }
+
+        public EnemyUnitState CheckTransition(EnemyUnitController owner)
+        {
+            return EnemyUnitState.Stun;
+        }
+    }
+}
