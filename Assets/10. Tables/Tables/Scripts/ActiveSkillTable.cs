9cdc0b6e689e3063fedb26e857856708f5f9c2d8
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-06-26 16:17:36 +0900

    feat : ActiveSkill을 직업별로 받아올 수 있는 기능 추가
    - ActiveSkill을 직업별로 받아올 수 있는 기능 추가

--- Assets/10. Tables/Tables/Scripts/ActiveSkillTable.cs
+++ Assets/10. Tables/Tables/Scripts/ActiveSkillTable.cs
@@ -7,12 +7,30 @@ public class ActiveSkillTable : BaseTable<int, SkillData>
 {
     protected override string[] DataPath => new[] { "Assets/10. Tables/Skill/Active" };
 
+    public Dictionary<JobType, List<SkillData>> ActiveSkillByJob { get; private set; } = new Dictionary<JobType, List<SkillData>>();
+
     public override void CreateTable()
     {
         Type = GetType();
-        foreach (SkillData activeSkillData in dataList)
+        foreach (SkillData skillData in dataList)
         {
-            DataDic[activeSkillData.ID] = activeSkillData;
+            if (!DataDic.TryAdd(skillData.ID, skillData))
+            {
+                Debug.LogWarning($"중복된 Passive ID 감지: {skillData.ID} - {skillData.name}");
+            }
+
+            if (!ActiveSkillByJob.TryGetValue(skillData.jobType, out List<SkillData> activeSkillData))
+            {
+                activeSkillData = new List<SkillData>();
+                ActiveSkillByJob[skillData.jobType] = activeSkillData;
+            }
+
+            activeSkillData.Add(skillData);
         }
+    }
+
+    public List<SkillData> GetActiveSkillsByJob(JobType jobType)
+    {
+        return ActiveSkillByJob.TryGetValue(jobType, out List<SkillData> passiveSkills) ? passiveSkills : new List<SkillData>();
     }
 }
