e5e27d161b204a5a824196f63ec8d46970ae5abf
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-06-26 21:20:57 +0900

    feat : Turn State 추가

---
+++ Assets/2. Scripts/Interface/ITurnState.cs
@@ -0,0 +1,8 @@
+﻿public interface ITurnState
+{
+    void OnEnter(Unit unit);
+    void OnUpdate(Unit unit);
+    void OnExit(Unit unit);
+
+    ITurnState CheckTransition(Unit unit);
+}
---
+++ Assets/2. Scripts/State/TurnStats.cs
@@ -0,0 +1,79 @@
+﻿public class MoveToTargetState : ITurnState
+{
+    public void OnEnter(Unit unit)
+    {
+    }
+
+    public void OnUpdate(Unit unit)
+    {
+    }
+
+    public void OnExit(Unit unit)
+    {
+    }
+
+    public ITurnState CheckTransition(Unit unit)
+    {
+        return null;
+    }
+}
+
+public class ActState : ITurnState
+{
+    public void OnEnter(Unit unit)
+    {
+    }
+
+    public void OnUpdate(Unit unit)
+    {
+    }
+
+    public void OnExit(Unit unit)
+    {
+    }
+
+    public ITurnState CheckTransition(Unit unit)
+    {
+        return null;
+    }
+}
+
+public class ReturnState : ITurnState
+{
+    public void OnEnter(Unit unit)
+    {
+    }
+
+    public void OnUpdate(Unit unit)
+    {
+    }
+
+    public void OnExit(Unit unit)
+    {
+    }
+
+    public ITurnState CheckTransition(Unit unit)
+    {
+        return null;
+    }
+}
+
+// public class EndTurnState : ITurnState
+// {
+//     public void OnEnter(Unit unit)
+//     {
+//     }
+//
+//     public void OnUpdate(Unit unit)
+//     {
+//     }
+//
+//     public void OnExit(Unit unit)
+//     {
+//     }
+//
+//     public ITurnState CheckTransition(Unit unit)
+//     {
+//         return null;
+//     }
+// }
