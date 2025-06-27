6ae9fefca92b5b5fbce0f1fb2b6dfcec0d426060
mingyeon <74883641+sichelh@users.noreply.github.com>
2025-06-27 22:36:22 +0900

    fix : 배틀씬 스킬슬롯, start UI OnOff 설정
    - fix : 배틀씬 스킬슬롯, start UI OnOff 설정

--- Assets/2. Scripts/Manager/BattleManager.cs
+++ Assets/2. Scripts/Manager/BattleManager.cs
@@ -25,6 +25,7 @@ public class BattleManager : SceneOnlySingleton<BattleManager>
     {
         TurnHandler = new TurnHandler();
         SetAllUnits(PartyUnits.Concat(EnemyUnits).ToList());
+        UIManager.Instance.Open<BattleSceneStartButton>(); // 배틀씬 시작되면 켜져야함.
     }
 
     public void SetAllUnits(List<Unit> units)
@@ -36,6 +37,7 @@ public class BattleManager : SceneOnlySingleton<BattleManager>
     public void StartTurn()
     {
         TurnHandler.StartNextTurn();
+        UIManager.Instance.Close<BattleSceneStartButton>(); // 턴 시작되면 start 버튼 끄기
     }
 
     public void EndTurn()
@@ -53,6 +55,7 @@ public class BattleManager : SceneOnlySingleton<BattleManager>
         TurnHandler.RefillTurnQueue();
         CommandPlanner?.Clear(); // 턴 종료되면 전략 플래너도 초기화
         OnBattleEnd?.Invoke();
+        UIManager.Instance.Open<BattleSceneStartButton>(); // 턴 종료되면 start 버튼 켜기
     }
 
     public List<Unit> GetAllies(Unit unit)
--- Assets/2. Scripts/Sumin/BattleSceneStartButton.cs
+++ Assets/2. Scripts/Sumin/BattleSceneStartButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleSceneStartButton : MonoBehaviour
+public class BattleSceneStartButton : UIBase
 {
     public void OnStartButton()
     {
