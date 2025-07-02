313e91658368471ece62638800178c29334640c0
yoonjin1127 <129118497+yoonjin1127@users.noreply.github.com>
2025-07-02 16:02:52 +0900

    feat: Csv To Json / Json To SO Converter
    Csv To Json / Json To SO Converter

---
+++ Assets/Editor/Dialogue/DialogueGroupJson.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+[System.Serializable]
+public class DialogueGroupJson
+{
+    public string groupKey;
+    public DialogueMode mode;
+    public List<DialogueLine> lines;
+}
