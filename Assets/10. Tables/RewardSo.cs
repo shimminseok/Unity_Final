90edbea531677b53ee052cfd84eaee8d900db51c
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-07-10 10:12:19 +0900

    feat : 스테이지 클리어시 Reward 추가
    스테이지 클리어시 Reward 추가

---
+++ Assets/10. Tables/RewardSo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewRewardSo", menuName = "ScriptableObjects/Reward/Reward", order = 0)]
+public class RewardSo : ScriptableObject
+{
+    public string Id;
+    public int RewardGold;
+    public int RewardOpal;
+
+    //추후 Reward로 캐릭터를 지급해야할지 논의
+}
---
+++ Assets/2. Scripts/Manager/RewardManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardManager : Singleton<RewardManager>
+{
+    private RewardTable rewardTable;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (isDuplicated)
+            return;
+    }
+
+    private void Start()
+    {
+        rewardTable = TableManager.Instance.GetTable<RewardTable>();
+    }
+
+    public void GiveReward(string id)
+    {
+        RewardSo reward = rewardTable.GetDataByID(id);
+        if (reward != null)
+        {
+            if (reward.RewardGold > 0)
+            {
+                AccountManager.Instance.AddGold(reward.RewardGold);
+            }
+
+            if (reward.RewardOpal > 0)
+            {
+                AccountManager.Instance.AddOpal(reward.RewardOpal);
+            }
+        }
+    }
+}
