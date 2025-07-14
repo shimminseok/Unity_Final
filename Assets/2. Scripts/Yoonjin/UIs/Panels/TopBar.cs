c0767ebd29bc1e9c28b09604a7216e81aa26fd58
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-07-14 13:24:58 +0900

    feat : Topbar에 Gold 및 Opal 재화 추가
    Topbar에 Gold 및 Opal 재화 추가

--- Assets/2. Scripts/Yoonjin/UIs/Panels/TopBar.cs
+++ Assets/2. Scripts/Yoonjin/UIs/Panels/TopBar.cs
@@ -1,19 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TopBar : MonoBehaviour
 {
     [SerializeField] private Button backButton;
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI opalText;
 
     private void Awake()
     {
         backButton.onClick.AddListener(OnBackButtonClicked);
     }
 
+    private void Start()
+    {
+        AccountManager.Instance.OnGoldChanged += UpdateGoldText;
+        AccountManager.Instance.OnOpalChanged += UpdateOpalText;
+    }
+
     private void OnBackButtonClicked()
     {
         UIManager.Instance.CloseLastOpenedUI();
+    }
+
+
+    public void UpdateGoldText(int gold)
+    {
+        goldText.text = $"{gold:N0}";
+    }
+
+    public void UpdateOpalText(int opal)
+    {
+        opalText.text = $"{opal:N0}";
+    }
+
+
+    private void OnDisable()
+    {
+        AccountManager.Instance.OnGoldChanged -= UpdateGoldText;
+        AccountManager.Instance.OnOpalChanged -= UpdateOpalText;
     }
 }
