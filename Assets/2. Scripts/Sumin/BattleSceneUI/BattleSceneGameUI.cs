c8c794017f72e6840b356dcf28677821efc0e380
mingyeon <74883641+sichelh@users.noreply.github.com>
2025-07-22 09:01:52 +0900

    feat : 턴 시작 애니 구현
    - feat : 턴 시작 애니 구현

--- Assets/2. Scripts/Sumin/BattleSceneUI/BattleSceneGameUI.cs
+++ Assets/2. Scripts/Sumin/BattleSceneUI/BattleSceneGameUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,14 +9,78 @@ public class BattleSceneGameUI : MonoBehaviour
 {
     [SerializeField] private Button startBtn;
     [SerializeField] private GameObject playingImage;
-    [SerializeField] private TextMeshProUGUI turnText;
 
     private BattleManager battleManager;
+
+    [Header("상단에 있는 턴 UI")]
+    [SerializeField] private CanvasGroup TurnTopUI;
+    [SerializeField] private TextMeshProUGUI turnText;
+
+    [Header("두트윈으로 재생되는 턴 UI")]
+    [SerializeField] private CanvasGroup TurnAniUI;
+    [SerializeField] private RectTransform backgroundRect;
+    [SerializeField] private RectTransform titleTextRect;
+    [SerializeField] private TextMeshProUGUI turnAniText;
+    [SerializeField] private TextMeshProUGUI turnDescriptionText;
+
+    [SerializeField] private float slideDuration;
+    [SerializeField] private float scaleDuration;
+    [SerializeField] private Vector2 slideOffset; // 아래에서 위로갈 때 위치
+    [SerializeField] private float fadeInDuration;
+    [SerializeField] private float fadeOutDuration;
+    [SerializeField] private float turnAniDuration;
 
     private void OnEnable()
     {
         battleManager = BattleManager.Instance;
         battleManager.OnBattleEnd += UpdateTurnCount;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(WaitForLoading());
+    }
+
+    private IEnumerator WaitForLoading()
+    {
+        yield return null; // 로딩 끝나고 나와야될거같은데 어떻게?
+        PlayTurnIntroAnimation(false);
+    }
+
+    // 턴 UI 애니메이션
+    public void PlayTurnIntroAnimation(bool isBattleStart)
+    {
+        turnDescriptionText.text = isBattleStart ? "전투 시작" : "전략 선택";
+        
+        // 초기 설정
+        TurnTopUI.gameObject.SetActive(false);
+        TurnAniUI.gameObject.SetActive(true);
+        TurnTopUI.alpha = 0f;
+        TurnAniUI.alpha = 0f;
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(TurnAniUI.DOFade(1f, fadeInDuration).SetEase(Ease.InOutSine));
+
+        seq.Join(titleTextRect.DOAnchorPosY(titleTextRect.anchoredPosition.y - slideOffset.y, slideDuration).SetEase(Ease.OutCubic));
+
+        backgroundRect.localScale = Vector3.one * 2.3f;
+        seq.Join(backgroundRect.DOScale(2f, scaleDuration).SetEase(Ease.OutBack).SetDelay(0.1f));
+
+        seq.AppendInterval(turnAniDuration);
+
+        seq.AppendCallback(() => TurnTopUI.gameObject.SetActive(true));
+
+        seq.Append(TurnAniUI.DOFade(0f, fadeOutDuration).SetEase(Ease.OutSine));
+        seq.Join(TurnTopUI.DOFade(1f, fadeInDuration).SetEase(Ease.InOutSine));
+
+        seq.AppendCallback (() => TurnAniUI.gameObject.SetActive(false));
+
+        // 애니 재생완료된 후에 전투 시작
+        if (isBattleStart)
+        {
+            seq.AppendCallback(() => InputManager.Instance.OnClickTurnStartButton());
+        }
     }
 
     public void ToggleActiveStartBtn(bool toggle)
@@ -31,12 +96,15 @@ public class BattleSceneGameUI : MonoBehaviour
 
     public void OnStartButton()
     {
-        InputManager.Instance.OnClickTurnStartButton();
+        PlayTurnIntroAnimation(true);
     }
 
     private void UpdateTurnCount()
     {
-        turnText.text = $"Turn {battleManager.TurnCount}";
+        string turn = $"Turn {battleManager.TurnCount}";
+        turnText.text = turn;
+        turnAniText.text = turn;
+        PlayTurnIntroAnimation(false);
     }
 
     private void OnDisable()
