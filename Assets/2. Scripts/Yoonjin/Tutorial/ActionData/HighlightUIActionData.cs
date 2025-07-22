8db5d8b7e0746bddda933643baa2f29fe9052d53
yoonjin1127 <129118497+yoonjin1127@users.noreply.github.com>
2025-07-22 15:27:04 +0900

    feat: 배틀 씬 이전의 캐릭터 장비 / 스킬 & 덱빌딩 & 출전 튜토리얼 로직
    배틀 씬 이전의 캐릭터 장비 / 스킬 & 덱빌딩 & 출전 튜토리얼 로직

--- Assets/2. Scripts/Yoonjin/Tutorial/ActionData/HighlightUIActionData.cs
+++ Assets/2. Scripts/Yoonjin/Tutorial/ActionData/HighlightUIActionData.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class HighlightUIActionData : TutorialActionData
 {
     public bool requireDoubleClick = false;  // 더블 클릭 필요 여부
+    public bool requireHold = false;         // 홀드 버튼 여부
     public string targetButtonName;          // 강조할 버튼
     public bool autoBlockOthers = true;      // 나머지 버튼 자동 차단 여부
 
--- Assets/2. Scripts/Yoonjin/Tutorial/ActionExecutor/HighlightUIExecutor.cs
+++ Assets/2. Scripts/Yoonjin/Tutorial/ActionExecutor/HighlightUIExecutor.cs
@@ -4,8 +4,12 @@ using UnityEngine.UI;
 public class HighlightUIExecutor : TutorialActionExecutor
 {
     private Button targetButton;
+
     private bool requireDoubleClick;
     private int clickCount = 0;
+
+    private bool requireLongPress;
+    private HoldPressDetector holdPressDetector;
 
     public override void Enter(TutorialActionData actionData)
     {
@@ -15,18 +19,36 @@ public class HighlightUIExecutor : TutorialActionExecutor
         requireDoubleClick = data.requireDoubleClick;
         clickCount = 0;
 
+        requireLongPress = data.requireHold;
+
         targetButton = GameObject.Find(data.targetButtonName)?.GetComponent<Button>();
+
         if (targetButton == null)
         {
             Debug.LogError($"[튜토리얼] '{data.targetButtonName}' 버튼을 찾을 수 없습니다.");
             return;
         }
 
-        TutorialUIBlocker.BlockAllExcept(targetButton.gameObject);
-        TutorialUIHighlighter.Highlight(targetButton.gameObject);
+        if (requireLongPress)
+        {
+            holdPressDetector = targetButton.gameObject.GetComponent<HoldPressDetector>();
+            if (holdPressDetector == null)
+                holdPressDetector = targetButton.gameObject.AddComponent<HoldPressDetector>();
 
-        // 클릭 이벤트에 반응해서 다음 단계로 진행
-        targetButton.onClick.AddListener(OnTargetButtonClicked);
+            holdPressDetector.requireHoldTime = 0.5f;
+            holdPressDetector.OnLongPress += OnLongPressed;
+        }
+        else
+        {
+            // 클릭 이벤트로 다음 단계 진행
+            targetButton.onClick.AddListener(OnTargetButtonClicked);
+        }
+
+        if (data.autoBlockOthers)
+        {
+            TutorialUIBlocker.BlockAllExcept(targetButton.gameObject);
+        }
+        TutorialUIHighlighter.Highlight(targetButton.gameObject);
     }
 
     private void OnTargetButtonClicked()
@@ -37,6 +59,11 @@ public class HighlightUIExecutor : TutorialActionExecutor
             return;
 
         // 다음 Step 진행
+        manager.CompleteCurrentStep();
+    }
+
+    private void OnLongPressed()
+    {
         manager.CompleteCurrentStep();
     }
 
