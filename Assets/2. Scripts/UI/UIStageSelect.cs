88b899f2a8290a87d110d36c605138b7344ecfae
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-07-24 21:13:34 +0900

    feat : 스테이지가 변경될때 아래쪽에 스테이지 이름 슬라이드와 슬롯이 바뀌도록 수정

--- Assets/2. Scripts/UI/UIStageSelect.cs
+++ Assets/2. Scripts/UI/UIStageSelect.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.Serialization;
+
 public class UIStageSelect : UIBase
 {
     [SerializeField] private RectTransform mapContent;
@@ -9,12 +12,27 @@ public class UIStageSelect : UIBase
     [SerializeField] private StageInfoPanel stageInfoPanel;
 
     [SerializeField] private List<StageSlot> stageSlots;
-    private Vector2 dragStartPos;
-    private Vector2 contentStartPos;
-
     [SerializeField] private List<RectTransform> cloudList;
+
+    [Header("ChapterName")]
+    [SerializeField] private RectTransform currentChapterNameRect;
+
+    [SerializeField] private TextMeshProUGUI currentChapterName;
+    [SerializeField] private RectTransform nextChapterNameRect;
+    [SerializeField] private TextMeshProUGUI nextChapterName;
+
+    private Vector2 offScreenRight;
+    private Vector2 center;
+    private Vector2 offScreenLeft;
+
+    private StageSO currentStage;
+
+    private int currentChpaterIndex = 0;
+
+    private StageTable StageTable => TableManager.Instance.GetTable<StageTable>();
     public void SetStageInfo(StageSO stage)
     {
+        currentStage = stage;
         PlayerDeckContainer.Instance.SetStage(stage);
         stageInfoPanel.SetStageInfo(stage, DeckSelectManager.Instance.GetSelectedDeck());
         stageInfoPanel.OpenPanel();
@@ -31,6 +49,7 @@ public class UIStageSelect : UIBase
             return;
         }
 
+        AccountManager.Instance.UpdateLastChallengedStageId(currentStage.ID);
         LoadSceneManager.Instance.LoadScene("BattleScene_Main");
     }
 
@@ -43,26 +62,34 @@ public class UIStageSelect : UIBase
     public override void Open()
     {
         base.Open();
-        int index = 0;
-        foreach (StageSO dataDicValue in TableManager.Instance.GetTable<StageTable>().DataDic.Values)
-        {
-            if (stageSlots.Count <= index)
-                break;
-
-            stageSlots[index++].Initialize(dataDicValue);
-        }
-
+        InitializeCapterNameText();
+        SetStageSlot();
+        
         foreach (RectTransform rectTransform in cloudList)
         {
             StartCloudLoop(rectTransform);
         }
     }
+    
     public override void Close()
     {
         base.Close();
         stageInfoPanel.ClosePanel();
     }
 
+    private void SetStageSlot()
+    {
+        int index = 0;
+        foreach (StageSO dataDicValue in StageTable.GetStagesByChapter(currentChpaterIndex))
+        {
+            if (stageSlots.Count <= index)
+            {
+                break;
+            }
+
+            stageSlots[index++].Initialize(dataDicValue);
+        }
+    }
     private void StartCloudLoop(RectTransform rect)
     {
         rect.anchoredPosition = new Vector2(0, rect.anchoredPosition.y);
@@ -74,12 +101,85 @@ public class UIStageSelect : UIBase
         float moveSpeed = Random.Range(10f, 30f);
         float delayTime = Random.Range(0f, 4f);
         rect.DOKill();
-        rect.DOAnchorPos(new Vector2(-(Screen.width + rect.sizeDelta.x), rect.anchoredPosition.y), moveSpeed)
+        rect.DOAnchorPos(new Vector2(-(Screen.width + rect.rect.width), rect.anchoredPosition.y), moveSpeed)
             .SetEase(Ease.Linear)
             .SetDelay(delayTime).OnComplete(() =>
             {
                 rect.anchoredPosition = new Vector2(0, rect.anchoredPosition.y);
                 MoveCloud(rect); // 재귀 호출로 반복
             });
+    }
+
+
+    private void InitializeCapterNameText()
+    {
+        currentChpaterIndex = AccountManager.Instance.LastClearedStageId / 1000000;
+        currentChapterName.text = Define.ChapterNameDictionary[currentChpaterIndex];
+        float width = currentChapterNameRect.rect.width;
+        center = Vector2.zero;
+        offScreenLeft = new Vector2(-width, 0);
+        offScreenRight = new Vector2(width, 0);
+        nextChapterNameRect.anchoredPosition = offScreenRight;
+    }
+
+    private void SlideToNextText(string newText)
+    {
+        currentChapterNameRect.DOKill();
+        nextChapterNameRect.DOKill();
+        nextChapterName.text = newText;
+
+        nextChapterNameRect.anchoredPosition = offScreenRight;
+
+        currentChapterNameRect.DOAnchorPos(offScreenLeft, 0.5f).SetEase(Ease.InOutCubic);
+
+        nextChapterNameRect.DOAnchorPos(center, 0.5f).SetEase(Ease.InOutCubic).OnComplete(() =>
+        {
+            (currentChapterNameRect, nextChapterNameRect) = (nextChapterNameRect, currentChapterNameRect);
+
+            (currentChapterName, nextChapterName) = (nextChapterName, currentChapterName);
+
+            nextChapterNameRect.anchoredPosition = offScreenRight;
+        });
+    }
+
+    private void SlideToPrevText(string newText)
+    {
+        currentChapterNameRect.DOKill();
+        nextChapterNameRect.DOKill();
+        nextChapterName.text = newText;
+
+        nextChapterNameRect.anchoredPosition = offScreenLeft;
+
+        currentChapterNameRect.DOAnchorPos(offScreenRight, 0.5f).SetEase(Ease.InOutCubic);
+
+        nextChapterNameRect.DOAnchorPos(center, 0.5f).SetEase(Ease.InOutCubic).OnComplete(() =>
+        {
+            (currentChapterNameRect, nextChapterNameRect) = (nextChapterNameRect, currentChapterNameRect);
+            (currentChapterName, nextChapterName) = (nextChapterName, currentChapterName);
+
+            nextChapterNameRect.anchoredPosition = offScreenLeft;
+        });
+    }
+
+    public void OnClickedRightChapterButton()
+    {
+        int nextIndex = currentChpaterIndex + 1;
+        if (Define.ChapterNameDictionary.TryGetValue(nextIndex, out string chapterName))
+        {
+            currentChpaterIndex = nextIndex;
+            SlideToNextText(chapterName);
+            SetStageSlot();
+        }
+    }
+
+    public void OnClickedLeftChapterButton()
+    {
+        int prevIndex = currentChpaterIndex - 1;
+        if (Define.ChapterNameDictionary.TryGetValue(prevIndex, out string chapterName))
+        {
+            currentChpaterIndex = prevIndex;
+            SlideToPrevText(chapterName);
+            SetStageSlot();
+        }
     }
 }
