112f34d813c97dde37400488f85391a251cc2fa2
mingyeon <74883641+sichelh@users.noreply.github.com>
2025-07-24 17:40:10 +0900

    fix : 가챠 슬롯들 수정
    - fix : 가챠 슬롯들 수정

--- Assets/2. Scripts/Sumin/Gacha/GachaUI/EquipmentGachaSlotUI.cs
+++ Assets/2. Scripts/Sumin/Gacha/GachaUI/EquipmentGachaSlotUI.cs
@@ -1,17 +1,26 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class EquipmentGachaSlotUI : MonoBehaviour
 {
     [SerializeField] private Image equipmentImage;
     [SerializeField] private TextMeshProUGUI equipmentNameText;
-    [SerializeField] private TextMeshProUGUI equipmentTierText; // 나중에 프레임으로 변경
+    [SerializeField] private Image itemSlotFrame;
+    [SerializeField] private List<GameObject> itemGradeStars;
+    [SerializeField] private List<Sprite> itemGradeSprites;
 
     public void Initialize(EquipmentItemSO equipment)
     {
         equipmentImage.sprite = equipment.ItemSprite;
         equipmentNameText.text = equipment.ItemName;
-        equipmentTierText.text = $"{equipment.Tier}";
+        itemSlotFrame.sprite = itemGradeSprites[(int)equipment.Tier];
+
+        for (int i = 0; i < itemGradeStars.Count; i++)
+        {
+            itemGradeStars[i].SetActive(i <= (int)equipment.Tier);
+        }
     }
 }
--- Assets/2. Scripts/Sumin/Gacha/GachaUI/SkillGachaSlotUI.cs
+++ Assets/2. Scripts/Sumin/Gacha/GachaUI/SkillGachaSlotUI.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,20 +6,30 @@ using UnityEngine.UI;
 public class SkillGachaSlotUI : MonoBehaviour
 {
     [SerializeField] private Image skillIamge;
+    [SerializeField] private Image skillTierFrame;
+    [SerializeField] private List<GameObject> skillGradeStars;
     [SerializeField] private TextMeshProUGUI skillNameText;
-    [SerializeField] private TextMeshProUGUI skillTierText;
+    [SerializeField] private GameObject duplicatedGO;
+    [SerializeField] private TextMeshProUGUI duplicatedText;
+    [SerializeField] private List<Sprite> itemGradeSprites;
 
     // 스킬 슬롯 내용 업데이트
     public void Initialize(ActiveSkillSO skill)
     {
+        duplicatedGO.SetActive(false);
         skillIamge.sprite = skill.SkillIcon;
         skillNameText.text = skill.skillName;
-        skillTierText.text = $"{skill.activeSkillTier}";
+        skillTierFrame.sprite = itemGradeSprites[(int)skill.activeSkillTier];
+        for (int i = 0; i < skillGradeStars.Count; i++)
+        {
+            skillGradeStars[i].SetActive(i <= (int)skill.activeSkillTier);
+        }
     }
 
-    // 스킬 슬롯 결과 보상 업데이트
+    // 스킬 슬롯 중복 보상 업데이트
     public void ShowCompensation(int compensation)
     {
-        skillTierText.text = $"{compensation} Opal";
+        duplicatedGO.SetActive(true);
+        duplicatedText.text = $"{compensation} Opal";
     }
 }
